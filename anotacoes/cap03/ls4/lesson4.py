c381b85d02183eeb808d58f757837b070bcb509d
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-04 08:10:57 -0300

    ontinuando lesson4

---
+++ anotacoes/cap03/ls4/lesson4.py
@@ -0,0 +1,17 @@
+
+# This Python file uses the following encoding: utf-8
+import os, sys
+
+# Quiz - 4
+# speed_of_light = 299792458.0 
+# cycles_per_second = 2700000000.0
+
+# print speed_of_light / cycles_per_second
+
+# Quiz 10
+# Escreva um código em Python que define a variável age (idade) e atribui a ela sua idade em anos inteiros, depois usa esse valor para imprimir o número de dias correspondente.
+
+age = 30
+days_in_year = 365.25
+
+print age * days_in_year
