b89549c7ff8edac4b7961dfffa13bd5ac51c5672
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-08 18:45:52 -0300

    comecando lesson 5

---
+++ anotacoes/cap03/ls5/lesson5.md
@@ -0,0 +1,4 @@
+# Anotações sobre python sobre - Cap03 - Lesson 4 - Variáveis e Strings
+## Onde manter sua atenção
+
+`=` - significa atribuição
---
+++ anotacoes/cap03/ls5/lesson5.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+# Quiz 2
+# Write Python code that prints out the number of hours in 7 weeks.
+
+# number_hours =  24
+# number_days = 7
+# number_weeks = 7
+
+# print (number_hours * number_days) * number_weeks
+
+# Quiz 3
+s = ""
+print ('a' + s)[1:]
+print s[0] + s[1:]
+print s + ''
+print s[0:]
+
+# Quiz 4
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+# print s[:3] + t[4:] # minha solução
