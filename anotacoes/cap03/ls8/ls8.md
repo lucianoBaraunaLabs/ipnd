035d18f8d2a24f86ab3009c20d69d61952ea884d
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-17 23:59:03 -0300

    finalizando ls8

--- anotacoes/cap03/ls8/ls8.md
+++ anotacoes/cap03/ls8/ls8.md
@@ -71,3 +71,16 @@ def biggest(a, b, c):
 print biggest(1, 2, 3)
 #>>> 3
 ```
+
+### Exempo de código para ender loop
+```
+def remove_spaces(text):
+    text_without_spaces = '' #empty string for now
+    while text != '':
+        next_character = text[0]
+        if next_character != ' ':    #that's a single space
+            text_without_spaces = text_without_spaces + next_character
+        text = text[1:]
+    return text_without_spaces
+print remove_spaces("hello my name is andy how are you?")
+```
--- anotacoes/cap03/ls8/ls8.py
+++ anotacoes/cap03/ls8/ls8.py
@@ -1,4 +1,5 @@
 
+# coding: utf-8
 # Quiz 6
 # Define a procedure, is_friend, that takes
 # a string as its input, and returns a
@@ -45,23 +46,42 @@
 #         else:
 #             return num3
 
-def biggest(a,b,c):
-    if a > b:
-        if a > c:
-            return a
-        else:
-            return c
-    else:
-        if b > c:
-            return b
-        else:
-            return c
+# def biggest(a,b,c):
+#     if a > b:
+#         if a > c:
+#             return a
+#         else:
+#             return c
+#     else:
+#         if b > c:
+#             return b
+#         else:
+#             return c
 
 
 
-print biggest(1, 2, 3)
-#>>> 3
+# print biggest(1, 2, 3)
+# #>>> 3
 
-print biggest(1, 3, 2)
+# print biggest(1, 3, 2)
 
-print biggest(3, 1, 2)
+# print biggest(3, 1, 2)
+
+# def remove_spaces(text):
+#     text_without_spaces = '' #empty string for now
+#     while text != '':
+#         next_character = text[0]
+#         if next_character != ' ':    #that's a single space
+#             text_without_spaces = text_without_spaces + next_character
+#         text = text[1:]
+#     return text_without_spaces
+# print remove_spaces("hello my name is andy how are you?")
+
+# Quiz 16
+# def print_numbers(n):
+#     i = 1
+#     while i < n:
+#         i += 1
+#         print i
+
+# print_numbers(3)
