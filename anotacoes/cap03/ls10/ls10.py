db139c6f633b85556c7662252bde92eb28fdbf86
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-19 19:26:57 -0300

    fazendo ls10

--- anotacoes/cap03/ls10/ls10.py
+++ anotacoes/cap03/ls10/ls10.py
@@ -7,35 +7,52 @@
 # https://pt.wikipedia.org/wiki/Mediana_%28estat%C3%ADstica%29
 # Make sure your procedure has a return statement.
 
-def bigger(a,b):
-    if a > b:
-        return a
-    else:
-        return b
+# def bigger(a,b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
 
-def biggest(a,b,c):
-    return bigger(a,bigger(b,c))
+# def biggest(a,b,c):
+#     return bigger(a,bigger(b,c))
     
-def median(a, b, c): # Checa a média de valor entre os números
-    big = biggest(a,b,c)
-    if big == a:
-        return bigger(b, c)
-    if big == b:
-        return bigger(a, c)
+# def median(a, b, c): # Checa a média de valor entre os números
+#     big = biggest(a,b,c)
+#     if big == a:
+#         return bigger(b, c)
+#     if big == b:
+#         return bigger(a, c)
+#     else:
+#         return bigger(a, b)
+
+
+# print(median(1,2,2))
+# #>>> 2
+
+# print(median(9,3,6))
+# #>>> 6
+
+# print(median(7,8,7))
+# #>>> 7
+
+#Quiz 5
+# Write code for the function random_noun, which takes in no inputs but outputs 
+# one of two nouns randomly. Use the randint function to generate a number 
+# from 0-1 and return a noun depending on whether the number is equal to 0 or 1. 
+# Feel free to experiment with different nouns, but for submission purposes return 
+# the string "sofa" if the number is 0, else return "llama".
+
+from random import randint
+
+def random_noun():
+    # your code here
+    sort_value = randint(0, 1)
+    if sort_value == 0:
+        return "sofa"
     else:
-        return bigger(a, b)
-
-
-print(median(1,2,2))
-#>>> 2
-
-print(median(9,3,6))
-#>>> 6
-
-print(median(7,8,7))
-#>>> 7
-
-
+        return "llama"
+    
+print random_noun()
 
 
 
