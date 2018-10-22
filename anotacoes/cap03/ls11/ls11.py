296798d5cdb3c5376ff65c831989781fd2fe7484
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-21 23:06:31 -0300

    finalizando lição 10 e iniciando a 11

---
+++ anotacoes/cap03/ls11/ls11.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+# Quiz 4 Dias do mês
+# Given the variable,
+
+# days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+# # define a procedure, how_many_days,
+# # that takes as input a number
+# # representing a month, and returns
+# # the number of days in that month.
+
+# def how_many_days(month_number):
+#  return days_in_month[month_number - 1]
+
+# print how_many_days(1)
+# #>>> 31
+
+# print how_many_days(9)
+# #>>> 30
+
+# Quiz 5 - Treinando Listas
+# Every entry in the following list is itself a list
+# nested_list = [['HTML', 'Hypertext Markup Language forms the structure of webpages'],
+#                ['CSS' , 'Cascading Style Sheets give pages style'],
+#                ['Python', 'Python is a programming language'],
+#                ['Lists', 'Lists are a data structure that let you organize information']]
+
+# first_concept = nested_list[0]
+
+# print "What do you think this will print?"
+# print first_concept 
+
+# print "Since first_concept was itself a list, we can access its elements"
+# first_title = first_concept[0]
+# first_description = first_concept[1]
+
+
+# print "What will this print?"
+# print first_title
+# print first_description
