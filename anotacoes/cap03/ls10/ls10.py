27ad4eb4866b56cd0c6f4a20fc91704399e9f17d
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-21 22:42:58 -0300

    finalizando lissão 10

--- anotacoes/cap03/ls10/ls10.py
+++ anotacoes/cap03/ls10/ls10.py
@@ -42,19 +42,95 @@
 # Feel free to experiment with different nouns, but for submission purposes return 
 # the string "sofa" if the number is 0, else return "llama".
 
+# from random import randint
+
+# def random_noun():
+#     # your code here
+#     sort_value = randint(0, 1)
+#     if sort_value == 0:
+#         return "sofa"
+#     else:
+#         return "llama"
+    
+# print random_noun()
+
+
+# Quiz 8 Transformador de palavras
+
+# Write code for the function word_transformer, which takes in a string word as input. 
+# If word is equal to "NOUN", return a random noun, if word is equal to "VERB", 
+# return a random verb, else return the first character of word. 
+
 from random import randint
 
+def random_verb():
+    random_num = randint(0, 1)
+    if random_num == 0:
+        return "run"
+    else:
+        return "kayak"
+        
 def random_noun():
-    # your code here
-    sort_value = randint(0, 1)
-    if sort_value == 0:
+    random_num = randint(0,1)
+    if random_num == 0:
         return "sofa"
     else:
         return "llama"
+
+# Minha resposta
+# def word_transformer(word):
+#     if word == 'NOUN':
+#         return random_noun()
+#     elif word == 'VERB':
+#         return random_verb()
+#     else:
+#         return word[0]
+
+# print word_transformer('XUXA')
+
+# Quiz 8 Processando Mad Libs
+# Let's put it all together. Write code for the function process_madlib, which takes in 
+# a string "madlib" and returns the string "processed", where each instance of
+# "NOUN" is replaced with a random noun and each instance of "VERB" is 
+# replaced with a random verb. You're free to change what the random functions
+# return as verbs or nouns for your own fun, but for submissions keep the code the way it is!
+
+# from random import randint
+
+# def random_verb():
+#     random_num = randint(0, 1)
+#     if random_num == 0:
+#         return "run"
+#     else:
+#         return "kayak"
+        
+# def random_noun():
+#     random_num = randint(0,1)
+#     if random_num == 0:
+#         return "sofa"
+#     else:
+#         return "llama"
+
+# def word_transformer(word):
+#     if word == "NOUN":
+#         return random_noun()
+#     elif word == "VERB":
+#         return random_verb()
+#     else:
+#         return word[0]
+        
+# def process_madlib(mad_lib):
+#     processed = ""
+#     processed = mad_lib.replace("NOUN",word_transformer("NOUN"))
+#     processed = processed.replace("VERB",word_transformer("VERB"))
+#     return processed 
     
-print random_noun()
+# test_string_1 = "This is a good NOUN to use when you VERB your food"
+# test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
+# print process_madlib(test_string_1)
+# print process_madlib(test_string_2)
 
 
-
+    
 
 
