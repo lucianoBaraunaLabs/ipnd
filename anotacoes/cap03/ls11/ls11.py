f6d7887e56f9bbdf809ce3621e2d6cc96c0e6cf1
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-23 18:01:48 -0300

    finalizated ls11 quiz 28

--- anotacoes/cap03/ls11/ls11.py
+++ anotacoes/cap03/ls11/ls11.py
@@ -53,3 +53,87 @@
 # # by accessing the list
 
 # print countries[1][1]
+
+#Quiz 26
+# Define a procedure, sum_list,
+# that takes as its input a
+# list of numbers, and returns
+# the sum of all the elements in
+# the input list.
+
+
+
+# def sum_list(listNumber):
+#     result = 0
+#     for n in listNumber:
+#         result += n
+#     return result
+        
+ 
+# print sum_list([1, 7, 4])
+# #>>> 12
+
+# print sum_list([9, 4, 10])
+# #>>> 23
+
+# print sum_list([44, 14, 76])
+# #>>> 134
+
+#Quiz 27
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase 
+# letter 'U'.
+
+# def measure_udacity(list):
+#     count = 0
+#     for e in list:
+#         if e[0] == 'U':
+#             count = count + 1
+#     return count
+
+
+
+# print measure_udacity(['Dave','Sebastian','Katy'])
+# #>>> 0
+
+# print measure_udacity(['Umika','Umberto'])
+# #>>> 2
+
+#Quiz 28
+# Define a procedure, find_element,
+# that takes as its inputs a list
+# and a value of any type, and
+# returns the index of the first
+# element in the input list that
+# matches the value.
+
+# If there is no matching element,
+# return -1.
+
+# Opção com while
+# def find_element(p, t):
+#     i = 0
+#     while i < len(p):
+#         if p[i] == t:
+#             return i
+#         i= i + 1
+#     return -1
+
+def find_element(p, t):
+    i = 0
+    for e in p:
+        if e == t:
+            return i
+        i = i + 1
+    return -1
+
+
+
+print find_element([1,2,3],3)
+#>>> 2
+
+print find_element(['alpha','beta'],'gamma')
+#>>> -1
