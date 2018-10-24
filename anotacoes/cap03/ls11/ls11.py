3621ed285cc57d0d3a980b34c8ea03767b5a41a1
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-24 18:22:39 -0300

    terminando lição 11

--- anotacoes/cap03/ls11/ls11.py
+++ anotacoes/cap03/ls11/ls11.py
@@ -122,18 +122,41 @@
 #         i= i + 1
 #     return -1
 
-def find_element(p, t):
-    i = 0
-    for e in p:
-        if e == t:
-            return i
-        i = i + 1
-    return -1
+# def find_element(p, t):
+#     for e in p:
+#         if e == t:
+#             return i
+#         i = i + 1
+#     return -1
 
 
 
-print find_element([1,2,3],3)
-#>>> 2
+# print find_element([1,2,3],3)
+# #>>> 2
 
-print find_element(['alpha','beta'],'gamma')
-#>>> -1
+# print find_element(['alpha','beta'],'gamma')
+# #>>> -1
+
+# Quiz 29
+# # Define a procedure, find_element,
+# # using index that takes as its
+# # inputs a list and a value of any
+# # type, and returns the index of
+# # the first element in the input
+# # list that matches the value.
+
+# # If there is no matching element,
+# # return -1.
+
+# def find_element(p, t):
+#    if t in p:
+#        return p.index(t)
+#    return -1
+
+
+
+# print find_element([1,2,3],3)
+# #>>> 2
+
+# print find_element(['alpha','beta'],'gamma')
+# #>>> -1
