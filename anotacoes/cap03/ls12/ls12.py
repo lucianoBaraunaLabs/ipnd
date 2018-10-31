db0ab654d3c247cf8d648b00e6695527127e1e04
Luciano Baraúna <baraunaluciano@gmail.com>
2018-10-31 18:08:08 -0300

    continuando aula 12

--- anotacoes/cap03/ls12/ls12.py
+++ anotacoes/cap03/ls12/ls12.py
@@ -193,6 +193,7 @@
 # print nextDay(2012, 12, 30) #=> (2013, 1, 1
 
 
+# Processo de solução de problema
 # Define a daysBetweenDates procedure that would produce the
 # correct output if there was a correct nextDay procedure.
 #
@@ -225,10 +226,7 @@ def dateIsBefore(year1, month1, day1, year2, month2, day2):
     return False
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
-    #Returns the number of days between year1/month1/day1
-    # and year2/month2/day2. Assumes inputs are valid dates
-    # in Gregorian calendar, and the first date is not after
-    # the second."""
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
     days = 0
     while dateIsBefore(year1, month1, day1, year2, month2, day2):
         year1, month1, day1 = nextDay(year1, month1, day1)
@@ -236,19 +234,4 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     return days
 
 
-print daysBetweenDates(2018, 10, 15, 2018, 10, 28)
-
-# def test():
-#     test_cases = [((2012,9,30,2012,10,30),30), 
-#                   ((2012,1,1,2013,1,1),360),
-#                   ((2012,9,1,2012,9,4),3)]
-    
-#     for (args, answer) in test_cases:
-#         result = daysBetweenDates(*args)
-#         if result != answer:
-#             print "Test with data:", args, "failed"
-#         else:
-#             print "Test case passed!"
-
-# test()
-
+print daysBetweenDates(2018, 1, 1, 2018, 12, 28)
