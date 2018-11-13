996f2b58c4ded9329d703bfa0aeeb880541a6e0a
Luciano Baraúna <baraunaluciano@gmail.com>
2018-11-13 00:44:49 -0200

    start ex 13

--- anotacoes/cap03/ls13/ls13.py
+++ anotacoes/cap03/ls13/ls13.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 
 
-def greatest(list_of_numbers):
-    if not list_of_numbers:
-        return 0
-    index = 1
-    value_actual = list_of_numbers[0]
-    while index < len(list_of_numbers):
-        if value_actual < list_of_numbers[index]:
-            value_actual = list_of_numbers[index]
-        index += 1
-    return value_actual
+# def greatest(list_of_numbers):
+#     if not list_of_numbers:
+#         return 0
+#     index = 1
+#     value_actual = list_of_numbers[0]
+#     while index < len(list_of_numbers):
+#         if value_actual < list_of_numbers[index]:
+#             value_actual = list_of_numbers[index]
+#         index += 1
+#     return value_actual
 
-print greatest([4,23,1])
-#>>> 23
-print greatest([])
-#>>> 0
+# print greatest([4,23,1])
+# #>>> 23
+# print greatest([])
+# #>>> 0
+
+string1 = "Yesterday, PERSON and I went to the PLACE. On our way, we saw a ADJECTIVE NOUN on a bike."
+string2 = "PLACE is located on the ADVERB side of Dublin, near the mainly ADJECTIVE areas of PLACE."
+list_of_words1 = string1.split(' ')
+list_of_words2 = string1.split(',')
+
+print list_of_words1
+print list_of_words2
