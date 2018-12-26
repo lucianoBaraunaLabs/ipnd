0733ba0c158e20e135bc05f424a6479d32a0077f
Luciano Baraúna <baraunaluciano@gmail.com>
2018-12-25 22:11:57 -0200

    lógica implementanda

--- anotacoes/projeto_quiz/fill-in-the-blanks.py
+++ anotacoes/projeto_quiz/fill-in-the-blanks.py
@@ -39,21 +39,66 @@ def pergunta_dificuldade():
 def obtem_frase_dificuldade(dificuldade):
     return frase_lacuna_resposta[dificuldade]["frase"], frase_lacuna_resposta[dificuldade]["lacuna"], frase_lacuna_resposta[dificuldade]["resposta"]
 
-def pergunta_resposta_lacuna(lacuna_atual, frase, indice_atual, resposta):
+# def pergunta_resposta_lacuna(lacuna_atual, frase, indice_atual, resposta):
+#     input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ? ')
+#     # if input_resposta == resposta[indice_atual]:
+#     #     print 'opa ta certo', frase.replace(lacuna_atual, input_resposta) 
+#     # else:
+#     #     print('Resposta errada! Tente Novamente.')
 
-    print lacuna_atual, frase, indice_atual, resposta
-    return lacuna_atual
-    # resposta_pergunta = raw_input('> A palavra do campo ' + lacuna + ' é ?')
-    # if resposta_pergunta === 
+#     # if input_resposta != resposta[indice_atual]:
+#     #     print('Resposta errada! Tente Novamente.')
+#     #     input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ? ')
+#     # else:
+#     #     print 'opa ta certo', frase.replace(lacuna_atual, input_resposta) 
+
+#     while indice_atual < len(resposta):
+#         if input_resposta == resposta[indice_atual]:
+#             print 'frase nova ', frase.replace(lacuna_atual, input_resposta)
+#             nova_frase = frase.replace(lacuna_atual, input_resposta)
+#             return nova_frase
+#         else:
+#             print('Resposta errada! Tente Novamente.')
+#             input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ? ')
+    
+    
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+#     print 'lacuna atual: ', lacuna_atual
+#     print 'frase: ', frase
+#     print 'indice_atual: ', indice_atual
+#     print 'resposta: ', resposta
+
+#     return lacuna_atual
+#     # resposta_pergunta = raw_input('> A palavra do campo ' + lacuna + ' é ?')
+#     # if resposta_pergunta === 
     
 def start():
     saudacoes()
     dificuldade = pergunta_dificuldade()
-    frase, lacunas, resposta = obtem_frase_dificuldade(dificuldade)
-    for indice_atual, lacuna_atual in enumerate(lacunas):
-        respostas = pergunta_resposta_lacuna(lacuna_atual, frase, indice_atual, resposta)
-    print 'aqui é frase ' + frase + '\n dificuldade aqui '+ dificuldade +  '\n resposta: ' + respostas
-
+    frase, lista_lacunas, lista_respostas = obtem_frase_dificuldade(dificuldade)
+    print frase
+    frase_nova = frase.replace(lista_lacunas[0], lista_respostas[0])
+    # resposta_quiz = pergunta_resposta_lacuna(lacuna_atual, frase, indice_atual, resposta)
+    for indice_atual, lacuna_atual in enumerate(lista_lacunas):
+        input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ? ')
+        while input_resposta != lista_respostas[indice_atual]:
+            print 'Resposta errada! Tente novamete.'
+            input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ? ')
+        print 'Resposta certa! Veja como está ficando a frase:'
+        frase_nova = frase_nova.replace(lista_lacunas[indice_atual], lista_respostas[indice_atual])
+        print frase_nova
 start()
 
 
