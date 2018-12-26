96447fe739e07a56f3e4bb7de234d84d77173397
Luciano Baraúna <baraunaluciano@gmail.com>
2018-12-26 17:20:08 -0200

    projeto finalizado

--- projeto_final_quiz/projeto_final_quiz.py
+++ projeto_final_quiz/projeto_final_quiz.py
@@ -4,22 +4,26 @@ frase_lacunas_resposta = {
     "iniciante": {
         "frase": "O rato roeu __1__ __2__ do __3__ de __4__",
         "lacunas": ["__1__", "__2__",  "__3__",  "__4__"],
-        "respostas":["a", "roupa", "rei", "roma"]
+        "respostas":["a", "roupa", "rei", "roma"],
+        "tentativas": 7
     },
     "facil": {
         "frase": "__1__ __2__ do pé __3__ pedro é __4__",
         "lacunas": ["__1__", "__2__", "__3__", "__4__"],
-        "respostas":["o", "peito", "do",  "preto" ]
+        "respostas":["o", "peito", "do",  "preto" ],
+        "tentativas": 6
     },
     "medio": {
         "frase": "O Brasil foi descoberto pelos __1__ em __2__ de __3__ de __4__",
         "lacunas": ["__1__", "__2__", "__3__", "__4__"],
-        "respostas":[ "portugueses", "22", "abril", "1500"]
+        "respostas":[ "portugueses", "22", "abril", "1500"],
+        "tentativas": 6
     },
     "dificil": {
         "frase": "A Bomba de __1__ e __2__ aconteceu em __3__ e __4__ de agosto em __5__",
         "lacunas": ["__1__", "__2__", "__3__", "__4__", "__5__"],
-        "respostas":["Hiroshima", "Nagasaki", "6", "9", "1945"]
+        "respostas":["Hiroshima", "Nagasaki", "6", "9", "1945"],
+        "tentativas": 7
     }
 }
 
@@ -63,35 +67,49 @@ def obtem_dados_jogo(dificuldade):
     string: Frase com lacunas, 
     list: Lista de Lacunas, 
     list: Lista de Respostas
+    list: Lista de Tentativas
 
     """
     configuracoes_jogo = frase_lacunas_resposta[dificuldade]
-    return configuracoes_jogo["frase"], configuracoes_jogo["lacunas"], configuracoes_jogo["respostas"]
+    return configuracoes_jogo["frase"], configuracoes_jogo["lacunas"], configuracoes_jogo["respostas"], configuracoes_jogo["tentativas"]
 
-def pergunta_resposta(frase, lista_lacunas, lista_respostas):
+def pergunta_resposta(frase, lista_lacunas, lista_respostas, qtd_tentativas):
     """
-    Realiza a pergunta, validando a resposta retornandoa
+    Realiza a pergunta
 
     Args:
 
     frase (string): Frase com lacunas
     lista_lacunas (list): Lista de lacunas
     lista_respostas (list): Lista de respostas
+    qtd_tentativas (int): Número de tentaivas
 
     Return:
     (string): Resposta
 
     """
     frase_nova = frase.replace(lista_lacunas[0], lista_respostas[0])
+    limite_tentativas = qtd_tentativas
+
     for indice_atual, lacuna_atual in enumerate(lista_lacunas):
-        input_resposta = raw_input('> A palavra do campo ' + lacuna_atual + ' é ? \n')
+        input_resposta = raw_input('> A palavra do campo ' + lacuna_atual + ' é ? \n').lower()
+
+        # Validação de tentativas
         while input_resposta != lista_respostas[indice_atual]:
-            print '> Resposta errada! Tente novamete.'
-            input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ?\n')
+            if limite_tentativas <= 1:
+                print '> Você excedeu o limite. Por favor começo novamente'
+                return
+            else:
+                limite_tentativas = limite_tentativas - 1
+                print '> Resposta errada! Tente novamete.'
+                print '> Você tem mais ' + str(limite_tentativas) + ' chances de errar!\n'
+                print frase_nova
+                input_resposta = raw_input('A palavra do campo ' + lacuna_atual + ' é ?\n').lower()
+        
         print '> Resposta certa! Veja como está ficando a frase:\n'
+        limite_tentativas = limite_tentativas
         frase_nova = frase_nova.replace(lista_lacunas[indice_atual], lista_respostas[indice_atual])
         print frase_nova
-
     
 def start():
     """
@@ -99,9 +117,9 @@ def start():
     """
     saudacoes()
     dificuldade = pergunta_dificuldade()
-    frase, lista_lacunas, lista_respostas = obtem_dados_jogo(dificuldade)
+    frase, lista_lacunas, lista_respostas, tentativas = obtem_dados_jogo(dificuldade)
     print frase
-    pergunta_resposta(frase, lista_lacunas, lista_respostas)
+    pergunta_resposta(frase, lista_lacunas, lista_respostas, tentativas)
 start()
 
 
